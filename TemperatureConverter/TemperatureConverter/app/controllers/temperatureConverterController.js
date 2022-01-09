1192555b42fb270666bb32979514a03b52fae26b
aamnaq-github <68397945+aamnaq-github@users.noreply.github.com>
2022-01-10 00:38:01 +0500

    Add files via upload

---
+++ TemperatureConverter/TemperatureConverter/app/controllers/temperatureConverterController.js
@@ -0,0 +1,32 @@
+﻿angular.module('app.controllers', []).
+    controller('temperatureConverterController', function ($scope, weatherDetailsAPIService) {
+        $scope.fahrenheit = 0.00;
+        $scope.celsius = 0.00;
+        $scope.weatherData = [];
+
+        $scope.toFahrenheit = function() {
+            $scope.fahrenheit = ($scope.celsius * 1.8) + 32
+        };
+
+        $scope.toCelsius = function() {
+            $scope.celsius = ($scope.fahrenheit - 32) / 1.8
+        };
+
+        weatherDetailsAPIService.getWeather().then(function successCallback(response) {
+            $scope.weatherData = response.data;
+        });
+
+        $scope.getWeather = function () {
+            $scope.result = $scope.weatherData.filter(p => p.name.toLowerCase() == $scope.city.toLowerCase())[0];
+            if ($scope.result == null) {
+                $scope.temperature = "No data found."
+                $scope.wind = null;
+                $scope.humidity = null;
+            }
+            else {
+                $scope.temperature = $scope.result.temperature.split(' ')[0] + "° " + $scope.result.temperature.split(' ')[1];
+                $scope.wind = "Wind: " + $scope.result.wind;
+                $scope.humidity = "Humidity: " + $scope.result.humidity;
+            }
+        };
+    });
---
+++ TemperatureConverter/TemperatureConverter/app/services/weatherDetailsService.js
@@ -0,0 +1,15 @@
+﻿angular.module('app.services', []).
+    factory('weatherDetailsAPIService', function ($http) {
+
+        var weatherDetailsAPI = {};
+
+        weatherDetailsAPI.getWeather = function () {
+            /*return $http({
+                method: 'GET',
+                url: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?unitGroup=metric&contentType=json'*/
+
+            return $http.get('app/weatherData.json');
+        };
+
+        return weatherDetailsAPI;
+    });
